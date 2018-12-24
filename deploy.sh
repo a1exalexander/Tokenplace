734404ef1e19ef8ef3e043b2d09cef9ed7f43e56
Alexander <40409129+a1exalexander@users.noreply.github.com>
2018-12-24 23:41:16 +0200

    deploy

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+
+# если вы публикуете на пользовательский домен
+# echo 'www.example.com' > CNAME
+
+git add -A
+git commit -m 'deploy'
+
+# если вы публикуете по адресу https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:a1exalexander/TokenPlace.git master:gh-pages
+
+
